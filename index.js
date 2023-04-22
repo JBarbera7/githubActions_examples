74ecbb99b814eb4e263caed5f3723de59063ba79
JBarbera7 <thetvkickerpvp@gmail.com>
2023-04-22 20:41:19 +0200

    Add send-telegram-message action

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(TOKEN, {polling: true});
-
-
-  bot.sendMessage(chatId, 'Hola, soy un bot');
-  bot.sendMessage(chatId, 'Workflow ejecutado correctamente tras el ultimo commit. Saludos '. NOMBRE);
- 
-console.log('Mensaje enviado')
---
+++ telegram.js
@@ -0,0 +1,15 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+
+const bot = new TelegramBot(token);
+
+async function sendMessage(name) {
+  const message = `Workflow ejecutado correctamente tras el último commit. Saludos ${name}`;
+  await bot.sendMessage(chatId, message);
+  console.log('Mensaje enviado');
+}
+
+module.exports = { sendMessage };
