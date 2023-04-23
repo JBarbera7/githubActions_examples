d9337e1827b788b648721581da3e2315511c4f69
JBarbera7 <thetvkickerpvp@gmail.com>
2023-04-23 19:45:45 +0200

    meme

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,37 +1,28 @@
 const fs = require('fs');
-const { MemeGenerator } = require('meme-generator');
+
 
 const frase_positiva = "Los tests han funcionado y lo sabes";
 const frase_negativa = "Los tests han fallado y lo sabes";
-const resultado_tests = true; // Ejemplo, cambiar a false para probar el resultado negativo
+const resultado_tests = true; // Cambia esto para simular diferentes resultados de los tests
 
-const memeTexto = resultado_tests ? frase_positiva : frase_negativa;
+let texto = frase_negativa;
+if (resultado_tests) {
+  texto = frase_positiva;
+}
 
-// Crear el meme con meme-generator
-const memeGenerator = new MemeGenerator();
-memeGenerator.generateMeme('./ejemplo.jpg', memeTexto)
-  .then((memeBuffer) => {
-    // Leer el contenido actual del archivo readme.md
-    fs.readFile('./readme.md', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      
-      // Agregar el meme al contenido
-      const nuevoContenido = data + `\n\n![Meme generado aleatoriamente](${memeBuffer.toString('base64')})`;
-      
-      // Escribir el contenido modificado en el archivo
-      fs.writeFile('./readme.md', nuevoContenido, (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        
-        console.log('Meme añadido al readme');
-      });
-    });
-  })
-  .catch((err) => {
-    console.error(err);
+generateMeme({
+  imageUrl: 'https://memegen.link/images/buzz/mocking-buzz.png', // Cambia esto para utilizar una imagen diferente
+  text: texto
+}).then((buffer) => {
+  fs.appendFile('README.md', `\n\n![Meme](data:image/png;base64,${buffer.toString('base64')})\n`, function (err) {
+    if (err) throw err;
+    console.log('Meme añadido al readme');
   });
+}).catch((error) => {
+  console.log(error);
+});
+
+
+
+
+
