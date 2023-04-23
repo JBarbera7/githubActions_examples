ffd066d3c4f0800c517dbc762013508c178b7dfd
JBarbera7 <thetvkickerpvp@gmail.com>
2023-04-23 16:10:32 +0200

    meme

--- testmemes.js
+++ testmemes.js
@@ -1,68 +1,26 @@
-const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
+const memeGenerator = require('meme-generator');
 
-async function generateMeme(frase, canvasWidth, canvasHeight) {
-  const canvas = createCanvas(canvasWidth, canvasHeight);
-  const ctx = canvas.getContext('2d');
+function addMemeToReadme(frase_positiva, frase_negativa, resultado_tests) {
+  // Generar el texto del meme aleatoriamente dependiendo del resultado de los tests
+  const texto_meme = resultado_tests ? frase_positiva : frase_negativa;
+  
+  // Generar el meme con la librería meme-generator
+  const meme = memeGenerator({
+    filePath: 'https://fotografias.lasexta.com/clipping/cmsimages02/2019/11/06/44963627-64F7-457B-9E18-189D99D449C4/69.jpg?crop=4:3,smart&width=1200&height=900&optimize=low&format=webply',
+    topText: 'Texto superior del meme',
+    bottomText: texto_meme,
+  });
 
-  // Load image
-  const image = await loadImage('meme.jpg');
-  ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+  // Leer el contenido del archivo readme.md
+  const readme = fs.readFileSync('./readme.md', 'utf8');
 
-  // Set text properties
-  ctx.font = 'bold 40pt Impact';
-  ctx.fillStyle = '#fff';
-  ctx.strokeStyle = '#000';
-  ctx.lineWidth = 3;
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
+  // Añadir el texto del meme al final del archivo readme.md
+  const nuevo_contenido = readme + '\n' + meme;
 
-  // Draw text
-  const words = frase.split(' ');
-  let line = '';
-  let y = canvasHeight / 2;
-  for (let n = 0; n < words.length; n++) {
-    const testLine = line + words[n] + ' ';
-    const metrics = ctx.measureText(testLine);
-    const testWidth = metrics.width;
-    if (testWidth > canvasWidth && n > 0) {
-      ctx.strokeText(line, canvasWidth / 2, y);
-      ctx.fillText(line, canvasWidth / 2, y);
-      line = words[n] + ' ';
-      y += 60;
-    } else {
-      line = testLine;
-    }
-  }
-  ctx.strokeText(line, canvasWidth / 2, y);
-  ctx.fillText(line, canvasWidth / 2, y);
+  // Escribir el nuevo contenido en el archivo readme.md
+  fs.writeFileSync('./readme.md', nuevo_contenido);
 
-  return canvas.toBuffer();
+  // Devolver el mensaje indicando que el meme ha sido añadido al readme.md
+  return "Meme añadido al readme";
 }
-
-async function run() {
-  try {
-    // Get inputs
-    const frasePositiva = process.env.FRASE_POSITIVA;
-    const fraseNegativa = process.env.FRASE_NEGATIVA;
-    const resultadoTests = process.env.RESULTADO_TESTS;
-
-    // Generate meme with positive or negative phrase based on test results
-    const frase = resultadoTests === 'success' ? frasePositiva : fraseNegativa;
-    const memeBuffer = await generateMeme(frase, 600, 400);
-
-    // Read and modify README.md file
-    let readme = fs.readFileSync('README.md', 'utf8');
-    readme += `\n\n![Meme](data:image/jpeg;base64,${memeBuffer.toString('base64')})\n\n`;
-
-    // Write modified README.md file
-    fs.writeFileSync('README.md', readme);
-
-    console.log('Meme añadido al readme');
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-}
-
-run();
