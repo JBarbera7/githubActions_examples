d9b4f873643a0659b05cede6527c95703b172391
JBarbera7 <thetvkickerpvp@gmail.com>
2023-04-23 20:00:51 +0200

    meme

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,31 +1,24 @@
 const core = require('@actions/core');
-const fs = require('fs');
-
-
-
+const fs = require('fs').promises;
 
 async function main() {
 
-  try {
-      const success_msg = core.getInput('frase_positiva').split(' ').join('_');
-      const error_msg = core.getInput('frase_negativa').split(' ').join('_');
-      const res = core.getInput('resultado_tests');
+    try {
+        const success_msg = core.getInput('frase_positiva').split(' ').join('_');
+        const error_msg = core.getInput('frase_negativa').split(' ').join('_');
+        const res = core.getInput('resultado_tests');
 
-      const img = res === 'success' ? 'stonks' : 'sarcasticbear';
-      const URL = `https://memegen.link/images/buzz/mocking-buzz.png`;
+        const img = res === 'success' ? 'stonks' : 'sarcasticbear';
+        const URL = `https://api.memegen.link/images/${img}/${res === 'success' ? success_msg : error_msg}.png`;
 
-      const old_readme = await fs.readFile('./OldREADME.md', 'utf8');
-      const new_readme = old_readme + `<img src="${URL}" />`;
-      
-      await fs.writeFile('./README.md', new_readme);
-      process.exit(0);
-  } catch (error) {
-      core.setFailed(error);
-  }
+        const old_readme = await fs.readFile('./OldREADME.md', 'utf8');
+        const new_readme = old_readme + `<img src="${URL}" />`;
+        
+        await fs.writeFile('./README.md', new_readme);
+        process.exit(0);
+    } catch (error) {
+        core.setFailed(error);
+    }
 }
 
 main();
-
-
-
-
